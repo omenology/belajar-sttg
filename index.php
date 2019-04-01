9180fa5155d12776479b9d282c539293e5db623a
omenology <ikbalsukabarang@gmail.com>
2019-04-01 09:29:15 +0700

    template

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 	$page = isset($_GET['page']) ? $_GET['page'] : false; 
-	include_once 'app/tamplate/header.php';
+	include_once 'app/template/header.php';
 	if($page){
 		include_once 'app/view/'.$page.'.php';	
 	}else{
 		include_once 'app/view/login.php';
 	}
 	
-	include_once 'app/tamplate/footer.php';
+	include_once 'app/template/footer.php';
