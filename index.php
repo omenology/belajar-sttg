dda2c40433ee9fffa7fedf413f5fe08386ef333d
omenology <ikbalsukabarang@gmail.com>
2019-05-12 12:27:35 +0700

    line6 index

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 	include_once 'app/function/config.php';
 	include_once 'app/function/parseUrl.php';
-
-	$url = parseUrl($_GET['url']);
+	$url = isset($_GET['url']) ? $_GET['url'] : "home"; 
+	$url = parseUrl($url);
 
 	$page = file_exists("app/view/$url[0].php") ? $url[0] : "home";
 	
