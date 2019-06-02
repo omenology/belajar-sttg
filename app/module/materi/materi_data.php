267684e0d6f0487e7110ac99f763fbaa42688f6f
omenology <ikbalsukabarang@gmail.com>
2019-06-02 19:43:17 +0700

    udate

--- app/module/materi/materi_data.php
+++ app/module/materi/materi_data.php
@@ -3,4 +3,9 @@ var_dump($_SESSION);
 $kdmk = $_SESSION['kode_mk'];
 $kelas = $_SESSION['kelas'];
 
-$query = mysqli_query($koneksi,"SELECT * FROM materi WHERE kode_mk = '$kdmk' AND kelas = '$kelas'");
+$query = "SELECT * FROM materi WHERE kode_mk = :kdmk AND kelas = :kelas";
+
+$db->query($query);
+$db->bind('kdmk',$kdmk);
+$db->bind('kelas',$kelas);
+$data = $db->resultSet();
