f0cce54b8842d396003e99b03db3d8a0cba02172
faisaldipkih <faisaldipkih@gmail.com>
2019-06-11 11:18:16 +0800

    salah

--- app/view/nilai.php
+++ app/view/nilai.php
@@ -1,56 +1,74 @@
-<?php
+<?php 
 
-	session_start();
-	$kode_mk = $_SESSION["kode_mk"];
+	include_once "app/module/nilai/nilai.php";
 
-	$npm = isset($url[1]) ? $url[1] : false;
-	$kd_mk = isset($url[2]) ? $url[2] : false;
-	$jumNilTug = 0;
-
-	// Nilai Absen
-	$absen = mysqli_query($koneksi, "SELECT absen FROM absen WHERE npm='$npm' AND kode_mk='$kode_mk' AND absen='1' AND status='done'");
-	$jumAbsen = mysqli_query($koneksi, "SELECT absen_ke FROM absen WHERE npm='$npm' AND kode_mk='$kode_mk'");
-		
-	$nilaiAbsen = @(mysqli_num_rows($absen) / mysqli_num_rows($jumAbsen) * 100);
-	//
-
-	// Nilai Tugas
-	$queryNilaiTugas = mysqli_query($koneksi, "SELECT nilai_tugas  FROM nilai_tugas WHERE npm='$npm' AND kode_mk='$kd_mk'");
-
-	foreach($queryNilaiTugas as $row){
-		$jumNilTug += $row["nilai_tugas"];
-	}
-	$nilaiTugas = @($jumNilTug / mysqli_num_rows($queryNilaiTugas));
-	// 
-
-	$jadwal_Ms = mysqli_query($koneksi, "SELECT jadwal_mahasiswa.npm, jadwal_mahasiswa.kode_mk, mata_kuliah.nama_mk FROM jadwal_mahasiswa JOIN mata_kuliah ON jadwal_mahasiswa.kode_mk=mata_kuliah.kode WHERE kode_mk='$kode_mk'");
-
-?>
-<?php if($npm) : ?>
+if($button) : ?>
 	<form action="<?php echo BASE_URL."app/module/nilai/nilai_masuk.php" ?>" method="post">
 		<input type="number" name="npm" value="<?= $npm ?>" readonly>
 		<input type="text" name="kode_mk" value="<?= $kd_mk ?>" readonly>
-		<input type="number" name="nilai_rata_tugas" value="<?= $nilaiTugas ?>" readonly>
-		<input type="number" name="absen" value="<?= $nilaiAbsen ?>" readonly>
-		<input type="number" name="uts" placeholder="Nilai UTS">
-		<input type="number" name="uas" placeholder="Nilai UAS">
-		<input type="submit" name="kirim">
+		<input type="number" name="nilai_rata_tugas" value="<?php if($button == "tilai"){ echo $nilaiTugas; }elseif($button == "edit"){ echo $nilai_rata_tugas; } ?>">
+		<input type="number" name="absen" value="<?php if($button == "tilai"){ echo $nilaiAbsen; }elseif($button == "edit"){ echo $nilai_rata_absen; } ?>">
+		<input type="number" name="uts" value="<?= $uts ?>" placeholder="Nilai UTS">
+		<input type="number" name="uas" value="<?= $uas ?>" placeholder="Nilai UAS">
+		<input type="submit" name="<?= $button ?>" value="<?= $button ?>">
 	</form>
 <?php endif; ?>
 
 <table>
 	<tr>
+	<?php if($status == "dosen") : ?>
 		<th>NPM</th>
-		<th>Kode Mata Kuliah</th>
+		<th>Nama Mahasiswa</th>
 		<th>Nama Matakuliah</th>
+	<?php endif; ?>
+		<th>Nilai Rata Tugas</th>
+		<th>Absen</th>
+		<th>UTS</th>
+		<th>UAS</th>
+		<th>Akhir</th>
+	<?php if($status == "dosen") : ?>
 		<th>Action</th>
+	<?php endif; ?>
 	</tr>
+<?php if($status == "dosen") : ?>
 	<?php foreach($jadwal_Ms as $row) : ?>
 		<tr>
 			<td><?= $row["npm"] ?></td>
-			<td><?= $row["kode_mk"] ?></td>
+			<td><?= $row["nama_mhs"] ?></td>
 			<td><?= $row["nama_mk"] ?></td>
-			<td><a href="<?php echo BASE_URL."nilai/$row[npm]/$row[kode_mk]"?>">Nilai</a></td>
+
+			<?php 
+				$nilai = mysqli_query($koneksi, "SELECT npm, kode_mk, nilai_rata_tugas, absen, uts, uas, akhir FROM nilai WHERE npm='$row[npm]' AND kode_mk='$row[kode_mk]'");
+
+				$rowNilai = mysqli_fetch_assoc($nilai);
+			?>
+			<?php if(mysqli_num_rows($nilai) == 0) : ?>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+				<td><a href="<?php echo BASE_URL."nilai/tilai/$row[npm]/$row[kode_mk]"?>">Nilai</a></td>
+			<?php else : ?>
+				<td><?= $rowNilai["nilai_rata_tugas"] ?></td>
+				<td><?= $rowNilai["absen"] ?></td>
+				<td><?= $rowNilai["uts"] ?></td>
+				<td><?= $rowNilai["uas"] ?></td>
+				<td><?= $rowNilai["akhir"] ?></td>
+				<td><a href="<?php echo BASE_URL."nilai/edit/$rowNilai[npm]/$rowNilai[kode_mk]/$rowNilai[nilai_rata_tugas]/$rowNilai[absen]/$rowNilai[uts]/$rowNilai[uas]"?>">Edit</a></td>
+			<?php endif; ?>
+
 		</tr>
 	<?php endforeach; ?>
+<?php elseif($status == "mahasiswa") : ?>
+	<?php foreach($nilaiMhs as $row) : ?>
+		<tr>
+			<td><?= $row["nilai_rata_tugas"] ?></td>
+			<td><?= $row["absen"] ?></td>
+			<td><?= $row["uts"] ?></td>
+			<td><?= $row["uas"] ?></td>
+			<td><?= $row["akhir"] ?></td>
+		</tr>
+	<?php endforeach; ?>
+<?php endif; ?>
 </table>
--- app/view/nilai_tugas.php
+++ app/view/nilai_tugas.php
@@ -14,26 +14,42 @@ if($npm) : ?>
 	<table class="centered">
 		<thead  class=" centered  indigo lighten-2">
 		<tr>
+		<?php if($id_user && $status == "dosen") : ?>
 			<th>NPM</th>
 			<th>Kode Mata Kuliah</th>
+		<?php endif; ?>
 			<th>No Tugas</th>
 			<th>File</th>
 			<th>Nilai</th>
-			<?php if($id_user && $status == "dosen") : ?>
+		<?php if($id_user && $status == "dosen") : ?>
 			<th>Action</th>
-			<?php endif; ?>
+		<?php endif; ?>
 		</tr>
 		</thead>
+	<?php if($status == "dosen") : ?>
 		<?php foreach($queryTugas as $rowTugas) : ?>
-		<tr>
-			<td><?= $rowTugas["npm"] ?></td>
-			<td><?= $rowTugas["kode_mk"] ?></td>
-			<td><?= $rowTugas["no_tugas"] ?></td>
-			<td><?= $rowTugas["file"] ?></td>
-			<?php
-				queryNilai( $rowTugas["npm"], $rowTugas["kode_mk"], $rowTugas["no_tugas"] );
-			?>
-		</tr>
+			<tr>
+				<td><?= $rowTugas["npm"] ?></td>
+				<td><?= $rowTugas["kode_mk"] ?></td>
+				<td><?= $rowTugas["no_tugas"] ?></td>
+				<td><?= $rowTugas["file"] ?></td>
+				<?php
+					queryNilai( $rowTugas["npm"], $rowTugas["kode_mk"], $rowTugas["no_tugas"] );
+				?>
+			</tr>
 		<?php endforeach; ?>
+	<?php elseif($status == "mahasiswa") : ?>
+		<?php foreach($tugasMhs as $row) : ?>
+			<tr>
+				<td><?= $row["no_tugas"] ?></td>
+				<td><?= $row["file"] ?></td>
+				<?php  
+					$nilaiTugas = mysqli_query($koneksi, "SELECT nilai_tugas FROM nilai_tugas WHERE npm='$row[npm]' AND kode_mk='$row[kode_mk]' AND no_tugas='$row[no_tugas]'");
+					$rowNilai = mysqli_fetch_assoc($nilaiTugas);
+				?>
+				<td><?= $rowNilai["nilai_tugas"] ?></td>
+			</tr>
+		<?php endforeach; ?>
+	<?php endif; ?>
 	</table>
 </div>
