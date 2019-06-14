345bf7ec3ff0e510d5813cd632fae42a2f27d6db
faisaldipkih <faisaldipkih@gmail.com>
2019-06-14 17:22:55 +0800

    repisi

--- app/view/nilai_tugas.php
+++ app/view/nilai_tugas.php
@@ -1,5 +1,4 @@
 <?php
-	
 	include_once "app/module/nilai_tugas/nilai_tugas.php";
 if($npm) : ?>
 <form action="<?php echo BASE_URL."app/module/nilai_tugas/nilai_tugas_masuk.php"?>" method="post">
@@ -13,43 +12,41 @@ if($npm) : ?>
 <div class="container">
 	<table class="centered">
 		<thead  class=" centered  indigo lighten-2">
-		<tr>
-		<?php if($id_user && $status == "dosen") : ?>
-			<th>NPM</th>
-			<th>Kode Mata Kuliah</th>
-		<?php endif; ?>
-			<th>No Tugas</th>
-			<th>File</th>
-			<th>Nilai</th>
-		<?php if($id_user && $status == "dosen") : ?>
-			<th>Action</th>
-		<?php endif; ?>
-		</tr>
+			<tr>
+				<?php if($id_user && $status == "dosen") : ?>
+					<th>NPM</th>
+					<th>Nama Mahasiswa</th>
+					<th>Kode Mata Kuliah</th>
+				<?php endif; ?>
+					<th>No Tugas</th>
+					<th>File</th>
+					<th>Nilai</th>
+				<?php if($id_user && $status == "dosen") : ?>
+					<th>Action</th>
+				<?php endif; ?>
+			</tr>
+		
+	<?php if($id_user && $status == "dosen") { ?>
+
+		<?php foreach($tugas as $row) { ?>
+			<tr>
+				<?php foreach($row as $rowTugas) { ?>
+					<td><?= $rowTugas ?></td>
+				<?php } ?>
+			</tr>
+		<?php } ?>
+
+	<?php }elseif($id_user && $status == "mahasiswa") { ?>
+
+		<?php foreach($tugasMhs as $row) { ?>
+			<tr>
+				<?php foreach($row as $rowTugas) { ?>
+					<td><?= $rowTugas ?></td>
+				<?php } ?>
+			</tr>
+		<?php } ?>
+
+	<?php } ?>
 		</thead>
-	<?php if($status == "dosen") : ?>
-		<?php foreach($queryTugas as $rowTugas) : ?>
-			<tr>
-				<td><?= $rowTugas["npm"] ?></td>
-				<td><?= $rowTugas["kode_mk"] ?></td>
-				<td><?= $rowTugas["no_tugas"] ?></td>
-				<td><?= $rowTugas["file"] ?></td>
-				<?php
-					queryNilai( $rowTugas["npm"], $rowTugas["kode_mk"], $rowTugas["no_tugas"] );
-				?>
-			</tr>
-		<?php endforeach; ?>
-	<?php elseif($status == "mahasiswa") : ?>
-		<?php foreach($tugasMhs as $row) : ?>
-			<tr>
-				<td><?= $row["no_tugas"] ?></td>
-				<td><?= $row["file"] ?></td>
-				<?php  
-					$nilaiTugas = mysqli_query($koneksi, "SELECT nilai_tugas FROM nilai_tugas WHERE npm='$row[npm]' AND kode_mk='$row[kode_mk]' AND no_tugas='$row[no_tugas]'");
-					$rowNilai = mysqli_fetch_assoc($nilaiTugas);
-				?>
-				<td><?= $rowNilai["nilai_tugas"] ?></td>
-			</tr>
-		<?php endforeach; ?>
-	<?php endif; ?>
 	</table>
 </div>
